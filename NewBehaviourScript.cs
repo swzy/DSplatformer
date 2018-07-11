43a75f7eb243bff232369a874b4dd166622fc7b2
nstubler1 <40697666+nstubler1@users.noreply.github.com>
2018-07-11 10:57:54 -0400

    Menu
    It is a base of a shell. If you run it the game is still playable in the background. but will refresh when you hit play.
    NewBehaviour is just the code to lanch the Main program.

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+	
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+
+}
